d3f1e892dbcbbe0e9525b12d30b7b58f83afd0b6
夏良文 <1636833810@qq.com>
2020-03-01 12:15:31 +0800

    修改banner

--- Banner/banner1/js/index.js
+++ Banner/banner1/js/index.js
@@ -1,47 +1,47 @@
 var data = [
   {
     title: "庆余年: 张若昀陈道明领衔群像传奇",
-    img: "../imgs/1.jpg",
+    img: "./imgs/1.jpg",
     bg: "rgb(114, 16, 3)"
   },
   {
     title: "边境线之冷焰: 边防警察异域追击",
-    img: "../imgs/2.jpg",
+    img: "./imgs/2.jpg",
     bg: "rgb(2, 10, 23)"
   },
   {
     title: "幸福三重奏: 郎朗吉娜甜蜜逛电玩城",
-    img: "../imgs/3.jpg",
+    img: "./imgs/3.jpg",
     bg: "rgb(65, 103, 104)"
   },
   {
     title: "心动的offer: 学霸上法庭激烈辩论",
-    img: "../imgs/4.jpg",
+    img: "./imgs/4.jpg",
     bg: "rgb(142, 176, 204)"
   },
   {
     title: "美食告白记: 赖美云曝年底参加艺考",
-    img: "../imgs/5.jpg",
+    img: "./imgs/5.jpg",
     bg: "rgb(175, 157, 133)"
   },
   {
     title: "我们的歌: 肖战张韶涵首度同台",
-    img: "../imgs/6.jpg",
+    img: "./imgs/6.jpg",
     bg: "rgb(29, 36, 178)"
   },
   {
     title: "精英律师: 靳东率律师军团守护正义",
-    img: "../imgs/7.jpg",
+    img: "./imgs/7.jpg",
     bg: "rgb(9, 84, 79)"
   },
   {
     title: "中华兵王: 特警硬核激战大毒枭",
-    img: "../imgs/8.jpg",
+    img: "./imgs/8.jpg",
     bg: "rgb(59,68,47)"
   },
   {
     title: "VIP7周年硬核福利: 刷片赢双人游",
-    img: "../imgs/9.jpg",
+    img: "./imgs/9.jpg",
     bg: "rgb(12, 136, 108)"
   }
 ];
